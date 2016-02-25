36ff9187283b27658650b1012df22b267a9c34c6
Manson Jones <manson.jones@gmail.com>
2016-02-25 07:50:49 -0800

    added GCDBlackbox and moved organized network code into folders.

---
+++ virtualTourist/FlickrConvenience.swift
@@ -0,0 +1,9 @@
+//
+//  FlickrConvenience.swift
+//  virtualTourist
+//
+//  Created by Manson Jones on 2/25/16.
+//  Copyright © 2016 Manson Jones. All rights reserved.
+//
+
+import Foundation
---
+++ virtualTourist/GCDBlackBox.swift
@@ -0,0 +1,15 @@
+//
+//  GCDBlackBox.swift
+//  virtualTourist
+//
+//  Created by Manson Jones on 2/25/16.
+//  Copyright © 2016 Manson Jones. All rights reserved.
+//
+
+import Foundation
+
+func performUIUpdatesOnMain(updates: () -> Void) {
+    dispatch_async(dispatch_get_main_queue()) {
+        updates()
+    }
+}
