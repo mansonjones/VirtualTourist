b5bcdbdcac7ee914f2d0597e275dc2c1b6ab0a20
Manson Jones <manson.jones@gmail.com>
2016-04-01 11:40:49 -0700

    code improvements, getting ready for submission to Udacity.

--- virtualTourist/TravelLocationsMapViewController.swift
+++ virtualTourist/TravelLocationsMapViewController.swift
@@ -11,7 +11,7 @@ import MapKit
 import UIKit
 import CoreData
 
-class TravelLocationsViewController: UIViewController,
+class TravelLocationsMapViewController: UIViewController,
     MKMapViewDelegate, NSFetchedResultsControllerDelegate {
     
     var selectedPin : Pin!
